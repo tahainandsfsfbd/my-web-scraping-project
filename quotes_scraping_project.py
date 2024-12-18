7cc8567c24f55991d2cc90428a73ae7dff5f6076
tahainan <154071645+tahainan5@users.noreply.github.com>
2024-12-18 10:23:47 +0300

    Update quotes_scraping_project.py

--- quotes_scraping_project.py
+++ quotes_scraping_project.py
@@ -6,7 +6,7 @@ url = "https://quotes.toscrape.com"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Tüm alıntıları ve yazar isimlerini çek
+# Pull all quotes and author names
 quotes = soup.find_all("div", class_="quote")
 data = []
 
@@ -15,8 +15,8 @@ for quote in quotes:
     author = quote.find("small", class_="author").text
     data.append({"quote": text, "author": author})
 
-# Veriyi JSON dosyasına kaydet
+# Save data to JSON file 
 with open("quotes.json", "w", encoding="utf-8") as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
 
-print("Veriler JSON dosyasına başarıyla kaydedildi :)")
+print("Data saved to JSON file successfully :)")
